d9a867b3dea3055a97d4bc554b20df68f572b1f7
user.email <jobayer.ahmed109@gmail.com>
2024-02-14 13:12:31 +0600

    Conditional redder the history section

--- src/components/History/HistorySection.jsx
+++ src/components/History/HistorySection.jsx
@@ -1,28 +1,37 @@
 /* eslint-disable react/prop-types */
 import PropTypes from "prop-types";
 import HistoryList from "./HistoryList";
+import "./history.css";
 
 const HistorySection = ({ histories, restoreHistory, handleRestore }) => {
+  console.log(histories);
   return (
-    <div>
-      <h4>Operation Histories</h4>
-      {histories.length === 0 ? (
-        <p>
-          <small>No Histories Found</small>
-        </p>
-      ) : (
-        <ul>
-          {histories.map((historyItem) => (
-            <HistoryList
-              key={historyItem.id}
-              disabled={restoreHistory === historyItem.id}
-              handleRestore={handleRestore}
-              historyItem={historyItem}
-            />
-          ))}
-        </ul>
+    <>
+      {histories.length !== 0 && (
+        <div>
+          <div className="history__header">
+            <h4 className="history__title">Operation Histories</h4>
+            <div className="line"></div>
+          </div>
+          {histories.length === 0 ? (
+            <p className="no__history">
+              <small>No Histories Found</small>
+            </p>
+          ) : (
+            <ul>
+              {histories.map((historyItem) => (
+                <HistoryList
+                  key={historyItem.id}
+                  disabled={restoreHistory === historyItem.id}
+                  handleRestore={handleRestore}
+                  historyItem={historyItem}
+                />
+              ))}
+            </ul>
+          )}
+        </div>
       )}
-    </div>
+    </>
   );
 };
 
